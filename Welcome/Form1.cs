42ac700af74cc2f643681bb634c98a13627611e8
luciano060999martinez <62975046+luciano060999martinez@users.noreply.github.com>
2020-04-08 21:43:08 -0400

    Agregue boton y label

--- Welcome/Form1.cs
+++ Welcome/Form1.cs
@@ -16,5 +16,10 @@ namespace Welcome
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("HELLO");
+        }
     }
 }
